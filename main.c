64c60a7f61d01142208f0fb43eaf35b222d5d91c
Husain206 <husainal518@gmail.com>
2026-03-05 12:36:44 +0300

    usage

--- main.c
+++ main.c
@@ -1,42 +1,36 @@
 #include "interner.h"
+#include <time.h>
 
+#define MAX_SIZE 10000
+
+
+void generate_random_string(char *dest, size_t length) {
+    const char charset[] = "0123456789"
+                           "abcdefghijklmnopqrstuvwxyz"
+                           "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    size_t charset_size = sizeof(charset) - 1; 
+    
+    if (charset_size == 0) return;
+
+    for (size_t i = 0; i < length; i++) {
+        size_t index = rand() % charset_size; 
+        dest[i] = charset[index];
+    }
+    dest[length] = '\0';
+}
 int main(void){
-
-  // intern intern = {0};
+  srand((unsigned int)time(NULL));
+  size_t length = 10;
+  char random_string[length + 1]; 
   intern intern;
   init_interner(&intern);
-  int id0 = interner(&intern, "hello");
-  const char* v0 = view(&intern, id0);
-
-  int id1 = interner(&intern, ", world");
-  const char* v1 = view(&intern, id1);
-
-  int  id2 = interner(&intern, "husein");
-  const char* v2 = view(&intern, id2);
-
-  int  id3 = interner(&intern, "idk");
-  const char* v3 = view(&intern, id3);
-
-  int  id4 = interner(&intern, "what");
-  const char* v4 = view(&intern, id4);
-
-  int  id5 = interner(&intern, "hmm");
-  const char* v5 = view(&intern, id5);
-
-  int  id6 = interner(&intern, "bruh");
-  const char* v6 = view(&intern, id6);
-
-  int  id7 = interner(&intern, "fuck");
-  const char* v7 = view(&intern, id7);
-
-  int  id8 = interner(&intern, "i");
-  const char* v8 = view(&intern, id8);
-
-  int  id9 = interner(&intern, "lol");
-  const char* v9 = view(&intern, id9);
-
+  
   for(int i = 0; i < MAX_SIZE; i++){
-    printf("id: %d, v: %s\n", intern.pool[i].id, intern.pool[i].name);
+    generate_random_string(random_string, length);
+    interner(&intern, random_string);
+  }
+  for(int i = 0; i < intern.count; i++){
+    printf("id: %d, v: %s\n", intern.pool[i].id, intern.arr[i]);
   }
 
   free_interner(&intern);
